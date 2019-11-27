0e4c6e96b9be249f97740f09046b10e6fdd3d2d3
RC Keely <rckeely@gmail.com>
2019-11-27 17:43:11 +0100

    added flask and requirements

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
 
+# def create_layout():
+#     return
 
+app = dash.Dash(__name__)
+app.layout = html.Div(className="container",
+                                children=["Hello World"])
 
 if __name__ == "__main__":
-    print("Hello world")
+    app.run_server(port=8050, debug=True)
